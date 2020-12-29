5030f95662c434fc50d509ef0795b1e3e7bd67e0
Keefer Taylor <keefer@keefertaylor.com>
2020-12-28 23:00:33 -0800

    get compiler fully working

--- commands/1/command.py
+++ commands/1/command.py
@@ -2,8 +2,12 @@ import smartpy as sp
 
 # This command simply transfers 1 XTZ to KT1DtPbioHJfyKxF61PxErRNkJjDyn9aDfhf
 def command(self):
-  sp.transfer_operation(
+  transfer_operation = sp.transfer_operation(
     sp.unit,
     sp.tez(1), 
     sp.contract(None, sp.address("KT1DtPbioHJfyKxF61PxErRNkJjDyn9aDfhf")
   ).open_some())
+
+  operation_list = [ transfer_operation ]
+
+  sp.result(operation_list)
