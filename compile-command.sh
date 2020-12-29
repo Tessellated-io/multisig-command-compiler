b095ffe544954e16dd1fe518b30786436c731321
Keefer Taylor <keefer@keefertaylor.com>
2020-12-28 22:55:53 -0800

    better compiler out

--- compile-command.sh
+++ compile-command.sh
@@ -18,10 +18,17 @@ fi
 # Command ID
 COMMAND_ID=$1
 echo "Using command ID $COMMAND_ID"
+echo ""
 
 OUT_DIR="./$COMMAND_ID-out"
 echo "Writing to $OUT_DIR"
+echo ""
 
 echo "Compiling..."
 $SMART_PY_CLI compile-expression "./commands/$COMMAND_ID/command.py" "command" $OUT_DIR
 echo "Done!"
+echo ""
+
+echo "Michelson lambda:"
+cat "$COMMAND_ID-out/command_michelson.tz"
+echo ""
